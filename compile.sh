f2e60c50379a941a3e0647e3790892f8decd75e6
Toni500github <toni500minecraft@gmail.com>
2022-11-17 18:44:26 +0100

    Adding termux(aarch64) support

--- compile.sh
+++ compile.sh
@@ -3,6 +3,7 @@
 #Created by https://github.com/Toni500github
 #Used for https://github.com/Toni500github/musical_donut
 DIR=$(dirname $0)
+pkg=vlc
 
 printf "\e[1;34mChecking libvlc headers... \n\n"
 sleep 1
@@ -30,7 +31,7 @@ function compile() {
     printf "\e[1;31mdonut.c not found in this directory \n"
     exit 1
   else
-    gcc -Wimplicit-function-declaration $DIR/donut.c -o $DIR/donut.o -lm -lvlc
+    gcc $DIR/donut.c -o $DIR/donut.o -lm -lvlc
   fi
 
 ## this will detect if donut.o is there after compiling
@@ -42,7 +43,28 @@ function compile() {
   fi
 }
 
+function detect_termux() {
+  arch=$(uname -i)
+  if [ "$arch" == 'aarch64' ]; then
+    detect_libvlc_termux
+    compile
+  else
+    detect_libvlc
+    compile
+  fi
+}
+
+function detect_libvlc_termux() {
+  if [[ ! -d /data/data/com.termux/files/usr/include/vlc || ! -f /data/data/com.termux/files/usr/include/vlc/vlc.h ]]; then
+    printf "\e[1;31mPlease run pkg install vlc\n"
+    exit 1
+  else
+    printf "\e[1;32mlibvlc headers are installed! \n"
+    printf "\e[1;34mcompiling donut.c ... \n\n \e[0m"
+    sleep 1
+  fi
+}
+
 ## calls fuctions
-detect_libvlc
-compile
+detect_termux
 chmod +x $DIR/donut.o && cd $DIR && ./donut.o
