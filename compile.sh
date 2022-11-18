95322052ea37bf163bd034395685e57fc4a5f608
Toni500git <Toni500minecraft@gmail.com>
2022-11-18 13:40:08 +0100

    Updating compile.sh
    the new name of the donut compiled file, will be simply donut. now the script will if you have android and using termux. Big feature: adding -Wno-everything on gcc so you won't warnings but it will show errors if you got one

--- compile.sh
+++ compile.sh
@@ -5,11 +5,11 @@
 DIR=$(dirname $0)
 
 printf "\e[1;34mChecking libvlc headers... \n\n"
-sleep 1
+sleep 0.7
 
-## if donut.o exists already, it will remove it
-if [[ -f $DIR/donut.o ]]; then
-  rm $DIR/donut.o
+## if donut exists already, it will remove it
+if [[ -f $DIR/donut ]]; then
+  rm $DIR/donut
 fi
 
 ## check if you have installed libvlc-dev
@@ -19,8 +19,8 @@ function detect_libvlc() {
     exit 1
   else
     printf "\e[1;32mlibvlc headers are installed! \n"
-    printf "\e[1;34mcompiling donut.c ... \n\n \e[0m"
-    sleep 1
+    printf "\e[1;34mcompiling donut.c ... \n\n"
+    sleep 0.5
   fi
 }
 
@@ -30,21 +30,21 @@ function compile() {
     printf "\e[1;31mdonut.c not found in this directory \n"
     exit 1
   else
-    gcc $DIR/donut.c -o $DIR/donut.o -lm -lvlc
+    gcc $DIR/donut.c -o $DIR/donut -lm -lvlc -Wno-everything
   fi
 
-## this will detect if donut.o is there after compiling
-  if [[ ! -f $DIR/donut.o ]]; then
-    printf " \e[1;31mdonut.o not compiled successfully \n"
+## this will detect if donut is there after compiling
+  if [[ ! -f $DIR/donut ]]; then
+    printf " \e[1;31mdonut not compiled successfully \n"
     exit 1
   else
-    printf "\e[1;34mdonut.o compiled, now run you can ./donut.o \nStarting donut.o ...\e[0m\n"
+    printf "\e[1;34mdonut compiled, now run you can ./donut \nStarting donut ...\e[0m\n"
   fi
 }
 
 function detect_termux() {
-arch=$(uname -i)
-  if [[ $arch == aarch64 ]] || [[ $arch = unknown ]]; then
+arch=$(uname -o)
+  if [[ $arch == Android ]] && [[ -d /data/data/com.termux/files/usr/bin/ ]]; then
     detect_libvlc_termux
     compile
   else
@@ -59,11 +59,11 @@ function detect_libvlc_termux() {
     exit 1
   else
     printf "\e[1;32mlibvlc headers are installed! \n"
-    printf "\e[1;34mcompiling donut.c ... \n\n \e[0m"
-    sleep 1
+    printf "\e[1;34mcompiling donut.c ... \n\n"
+    sleep 0.7
   fi
 }
 
 ## calls fuctions
 detect_termux
-chmod +x $DIR/donut.o && cd $DIR && ./donut.o
+chmod +x $DIR/donut && cd $DIR && ./donut
